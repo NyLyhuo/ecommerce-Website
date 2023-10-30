e0f833f911fc1202d81a06c7003896c52242642b
nylyhour <lyhuony@gmail.com>
2023-10-30 16:25:06 +0700

    Create Navigation bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
-import Nav from './components/Nav'
+import Nav from './components/Nav';
+import Rout from './route';
+import { BrowserRouter } from 'react-router-dom';
 
-function App() {
+const App = () => {
   return (
     <>
-    <Nav />
+    <BrowserRouter>
+      <Nav />
+      <Rout />
+    </BrowserRouter>
     </>
   )
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import { FaTruckMoving } from 'react-icons/fa';
+import { AiOutlineUser } from 'react-icons/ai';
 import { AiOutlineHeart } from 'react-icons/ai';
 import { BsBagCheck } from 'react-icons/bs';
+import { CiLogin } from 'react-icons/ci';
+import { CiLogout } from 'react-icons/ci';
+import { Link } from 'react-router-dom';
 import './Nav.css'
 
-function Nav() {
+const Nav = () => {
   return (
     <>
         <div className='free'>
@@ -20,12 +24,45 @@ function Nav() {
                     <img src='./img/logo.svg' alt='logo'></img>
                 </div>
                 <div className='search_box'>
-                    <input type="text" value='' placeholder='Enter the Product Name' autoComplete='off'/>
+                    <input type="text" value='' placeholder='Search Your Product...' autoComplete='off'/>
                     <button>Search</button>
                 </div>
                 <div className='icon'>
-                    <AiOutlineHeart />
-                    <BsBagCheck />
+                    <div className='account'>
+                        <div className='user_icon'>
+                            <AiOutlineUser />
+                        </div>
+                        <p>Hello, User</p>
+                    </div>
+                    <div className='sec_icon'> 
+                        <Link to="/" className='link'><AiOutlineHeart /></Link>
+                        <Link to="/cart" className='link'><BsBagCheck /></Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div className='header'>
+            <div className='container'>
+                <div className='nav'>
+                    <ul>
+                        <li>
+                            <Link to='/' className='link'>Home</Link>
+                        </li>
+                        <li>
+                            <Link to='/product' className='link'>Product</Link>
+                        </li>
+                        <li>
+                            <Link to='/about' className='link'>About</Link>
+                        </li>
+                        <li>
+                            <Link to='/contact' className='link'>Contact</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className='auth'>
+                    <button><CiLogin /></button>
+                    <button><CiLogout /></button>
                 </div>
             </div>
         </div>
